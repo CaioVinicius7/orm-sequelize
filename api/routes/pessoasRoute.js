5deada8e75ab761e32750e0e85dc6f57068c365d
Caio Vinícius <caio1525pereira@gmail.com>
2021-09-27 12:06:55 -0300

    Crud de usuários completo

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
-const { pegaTodasPessoas } = require("../controllers/PessoaController");
 const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 // Rotas
 router.get("/pessoas", PessoaController.pegaTodasPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 
 module.exports = router;
