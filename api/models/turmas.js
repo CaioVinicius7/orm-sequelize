70a4e99e53f23f1307c8c49123ec806aaa879eb7
Caio Vinícius <caio1525pereira@gmail.com>
2021-09-30 15:35:49 -0300

    Adição de novas tabelas e relacionamentro entre tabelas

--- api/models/turmas.js
+++ api/models/turmas.js
@@ -3,33 +3,35 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pessoas extends Model {
+  class Turmas extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-
+      
       // Define o relacionamento em outra tabela
-      Pessoas.hasMany(models.Turmas, {
+      Turmas.hasMany(models.Matriculas, {
+        foreignKey: "turma_id"
+      });
+
+      // Recebe o relacionamento de outra tabela
+      Turmas.belongsTo(models.Pessoas, {
         foreignKey: "docent_id"
       });
-      
-      Pessoas.hasMany(models.Matriculas, {
-        foreignKey: "estudante_id"
+
+      Turmas.belongsTo(models.Niveis, {
+        foreignKey: "nivel_id"
       });
-      
+
     }
   };
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
+  Turmas.init({
+    data_inicio: DataTypes.DATEONLY
   }, {
     sequelize,
-    modelName: 'Pessoas',
+    modelName: 'Turmas',
   });
-  return Pessoas;
+  return Turmas;
 };
